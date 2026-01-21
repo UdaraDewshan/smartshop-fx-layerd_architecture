f9538eb7ff50009cd059cab86f9037c701314402
UdaraDewshan <dewudara@gmail.com>
2026-01-21 23:06:11 +0530

    add PlaceOrder.fxml

---
+++ src/main/java/controller/PlaceOrderController.java
@@ -0,0 +1,71 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class PlaceOrderController {
+
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnPlaceOrder;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colDiscount;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private Label lblNetPrice;
+
+    @FXML
+    private TableView<?> tblPlaceOrder;
+
+    @FXML
+    private TextField txtCustomerID;
+
+    @FXML
+    private TextField txtCustomerName;
+
+    @FXML
+    private TextField txtDiscount;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtItemDescription;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    void btnAddToCartAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderAction(ActionEvent event) {
+
+    }
+
+}
